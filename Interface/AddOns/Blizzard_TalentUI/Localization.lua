6333268b6822955e569a5f827691672e56bec600
Dencer <tdaddon@163.com>
2022-09-04 17:04:45 +0800

    3.4.0.45435

--- Interface/AddOns/Blizzard_TalentUI/Localization.lua
+++ Interface/AddOns/Blizzard_TalentUI/Localization.lua
@@ -5,8 +5,3 @@ for i=1, (PlayerTalentFrame.numTabs or 0) do
 	local tabName = "PlayerTalentFrameTab"..i;
 	_G[tabName.."Text"]:SetPoint("CENTER", tabName, "CENTER", 0, 5);
 end
-
--- Move talent stuff
-PlayerTalentFrameTalentPoints:SetPoint("RIGHT", "PlayerTalentFrameTalentPointsText", "LEFT", -3, 0);
-PlayerTalentFrameTalentPointsText:SetPoint("BOTTOMRIGHT", "PlayerTalentFrame", "BOTTOMLEFT", 252, 84);
-PlayerTalentFrameSpentPoints:SetPoint("TOP", "PlayerTalentFramePointsMiddle", "TOP", 0, -2);
